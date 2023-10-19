e1cfbd8a08a7e4c06f52846f311a571cf311b5f4
Savva Morozov <morozov.savvam04@yandex.ru>
2023-10-20 01:07:33 +0300

    added template cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function addCard(name, link) {
+  const cardElement = cardTemplate.content.cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+  
+  document.querySelector('.places__list').appendChild(cardElement);
+}
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => addCard(`${item.name}`, `${item.link}`))
