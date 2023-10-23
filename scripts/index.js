496dff4771699e30894718a186a7d95ec0a25dc3
Savva Morozov <morozov.savvam04@yandex.ru>
2023-10-23 19:01:05 +0300

    addCard function with deleteButton

--- scripts/index.js
+++ scripts/index.js
@@ -2,44 +2,26 @@
 const cardTemplate = document.querySelector('#card-template');
 let cards = [];
 
-// @todo: Функция создания карточки
+// Функция создания карточки
 function addCard(name, link) {
-  cards.push({name, link});
-}
+  const cardElement = cardTemplate.content.cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
-function deleteCard (name, link) {
-  cards.forEach((card) => {
-    let index = cards.indexOf(card);
-  
-    if (card.name === name && card.link === link) {
-      delete(cards[index]);
-    }
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+
+  deleteButton.addEventListener('click', () => {
+    cards.pop();
+    document.querySelector('.card').remove();
   })
+
+  document.querySelector('.places__list').appendChild(cardElement);
+  cards.push({name, link});
 }
 
 // Вывод карточек на страницу
 initialCards.forEach((card) => addCard(`${card.name}`, `${card.link}`));
 addCard('oleg', 'https://images.unsplash.com/photo-1682685797507-d44d838b0ac7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
-
-cards.forEach((card) => {
-  const cardElement = cardTemplate.content.cloneNode(true);
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-
-  cardElement.querySelector('.card__image').src = card.link;
-  cardElement.querySelector('.card__title').textContent = card.name;
-  deleteButton.addEventListener('click', () => {
-    deleteCard(`${card.name}`, `${card.link}`)
-    console.log('card deleted')
-  })
-
-  document.querySelector('.places__list').appendChild(cardElement);
-})
-
-// функция addCard добавляет card в массив cards
-// функция deleteCard удаляет card из массива
-// а в самом конце выводится содержимое массива cards
-
 
 // Реализация работы попапов
 //const closeButton = document.querySelectorAll('.popup__close');
