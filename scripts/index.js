7ee430410d3f8b64043e5ab5354f37fed1a83c25
NoKaLoKa <morozov.savvam04@yandex.ru>
2023-10-29 16:58:02 +0300

    change addCard

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,23 @@
 const cardTemplate = document.querySelector('#card-template');
 
 // Функция создания карточки
-function addCard(name, link) {
+function addCard(cardIndex, deleteCard) {
   const cardElement = cardTemplate.content.cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  deleteButton.addEventListener('click', (evt) => evt.target = deleteCard(deleteButton));
 
-  cardElement.querySelector('.card__image').src = link;
-  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = initialCards[cardIndex].link;
+  cardElement.querySelector('.card__title').textContent = initialCards[cardIndex].name;
 
   document.querySelector('.places__list').appendChild(cardElement);
-  deleteButton.addEventListener('click', () => deleteButton.parentElement.remove())
+}
+
+// Функция удаления карточки
+function deleteCard(deleteButton) {
+  const card = deleteButton.closest('.card');
+  card.remove();
 }
 
 // Вывод карточек на страницу
-initialCards.forEach((card) => addCard(`${card.name}`, `${card.link}`));
-addCard('oleg', 'https://images.unsplash.com/photo-1682685797507-d44d838b0ac7?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
-
-// Реализация работы попапов
-//const closeButton = document.querySelectorAll('.popup__close');
-
-//closeButton.forEach(item => item.addEventListener('click', () => profileAddPopup.classList.remove('popup_is-opened')))
-
-//const profileEditButton = document.querySelector('.profile__edit-button')
-//const profileEditPopup = document.querySelector('.popup_type_edit')
-
-//profileEditButton.addEventListener('click', () => profileEditPopup.classList.add('popup_is-opened'))
-
-//const profileAddButton = document.querySelector('.profile__add-button');
-//const profileAddPopup = document.querySelector('.popup_type_new-card');
-
-//profileAddButton.addEventListener('click', () => profileAddPopup.classList.add('popup_is-opened'))
-
-//const cardImageButton = document.querySelector('.popup__image');
-//const cardImagePopup = document.querySelector('.popup_type_image');
-
-//cardImageButton.addEventListener('click', () => cardImagePopup.classList.add('popup_is-opened'))
+initialCards.forEach((card) => addCard(initialCards.indexOf(card), deleteCard));
